7c3cf3dfde0f64d63bdc04a64ba324b6026e6f97
zayservl <zay.ser.vl@gmail.com>
2023-09-20 17:53:50 +0300

    ml dz 2

--- DB_script_DZ_3.py
+++ DB_script_DZ_3.py
@@ -27,3 +27,79 @@ finally:
         cursor.close()
         connection.close()
         print("Соединение с PostgreSQL закрыто")
+
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, concatenate, Conv2DTranspose
+
+# Определение функции создания U-Net модели
+def unet(input_size=(256, 256, 3)):
+    inputs = Input(input_size)
+    
+    # Сверточный блок 1
+    conv1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    # Сверточный блок 2
+    conv2 = Conv2D(128, 3, activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    # Сверточный блок 3
+    conv3 = Conv2D(256, 3, activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    # Сверточный блок 4
+    conv4 = Conv2D(512, 3, activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    # Сверточный блок 5
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    # Раскодирование и объединение блоков
+    up6 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(drop5)
+    merge6 = concatenate([drop4, up6], axis=3)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same')(merge6)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same')(conv6)
+
+    up7 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv6)
+    merge7 = concatenate([conv3, up7], axis=3)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same')(merge7)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same')(conv7)
+
+    up8 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv7)
+    merge8 = concatenate([conv2, up8], axis=3)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same')(merge8)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same')(conv8)
+
+    up9 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv8)
+    merge9 = concatenate([conv1, up9], axis=3)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same')(merge9)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same')(conv9)
+    
+    # Выходной слой
+    outputs = Conv2D(1, 1, activation='sigmoid')(conv9)
+
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+    return model
+
+# Создание экземпляра модели
+model = unet()
+
+# Компиляция модели
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Загрузка и подготовка данных (пропущено)
+
+# Обучение модели
+model.fit(train_data, train_labels, epochs=10, batch_size=32, validation_data=(val_data, val_labels))
+
+# Оценка модели
+test_loss, test_acc = model.evaluate(test_data, test_labels)
+print(f"Test accuracy: {test_acc}")
